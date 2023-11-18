6966c896986c2dbf25f0fbdb2212b3ea144f987a
Nico Kestel <=>
2023-11-18 13:12:18 +0100

    get experiment from cli argument

--- run_experiment.py
+++ run_experiment.py
@@ -1,5 +1,19 @@
+import sys
 from experiments import qm9_atomwise
 
+
+SUPPORTED_EXPERIMENTS = ['qm9_atomwise', 'qm9_dipole', 'md17']
+
+
 if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print('[ERROR] Provide experiment to execute! Choose one of {}'.format(SUPPORTED_EXPERIMENTS))
+        sys.exit(1)
+
+    expmt = globals()[sys.argv[1]]
     
-    qm9_atomwise.run()
+    print(expmt)
+    expmt.run()
+    
+    
