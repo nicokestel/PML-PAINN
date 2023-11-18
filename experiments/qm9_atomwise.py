b8e13c7107e9ae4467918efd52b8931aa8c47b84
Nico Kestel <=>
2023-11-18 12:59:24 +0100

    seperate experiments from execution

---
+++ experiments/qm9_atomwise.py
@@ -0,0 +1,96 @@
+import os
+import schnetpack as spk
+from schnetpack.datasets import QM9
+import schnetpack.transform as trn
+
+from nn import PaiNN
+# from schnetpack.representation import PaiNN
+from data_loader import load_data
+
+import torch
+import torchmetrics
+import pytorch_lightning as pl
+
+#if __name__ == '__main__':
+def run():
+    qm9atom = load_data('qm9',
+                        transformations=[
+                            trn.SubtractCenterOfMass(),
+                            trn.RemoveOffsets(QM9.U0, remove_mean=True, remove_atomrefs=True),
+                            trn.MatScipyNeighborList(cutoff=5.),
+                            trn.CastTo32()
+                        ],
+                        n_train=100000,
+                        n_val=1000,
+                        batch_size=100)
+
+    # some insides
+    atomrefs = qm9atom.train_dataset.atomrefs
+    print('U0 of hyrogen:', atomrefs[QM9.U0][1].item(), 'eV')
+    print('U0 of carbon:', atomrefs[QM9.U0][6].item(), 'eV')
+    print('U0 of oxygen:', atomrefs[QM9.U0][8].item(), 'eV')
+
+    means, stddevs = qm9atom.get_stats(
+        QM9.U0, divide_by_atoms=True, remove_atomref=True
+    )
+    print('Mean atomization energy / atom:', means.item())
+    print('Std. dev. atomization energy / atom:', stddevs.item())
+
+
+    # Model Setup (QM9)
+    cutoff = 5.  # Angstrom
+    n_atom_basis = 128
+    n_interactions = 3
+
+    pairwise_distance = spk.atomistic.PairwiseDistances() # calculates pairwise distances between atoms
+    # radial_basis = spk.nn.GaussianRBF(n_rbf=20, cutoff=cutoff)
+    painn = PaiNN(
+        n_atom_basis=       n_atom_basis,
+        n_interactions=     n_interactions,
+        radial_basis=       spk.nn.radial.GaussianRBF(n_rbf=20, cutoff=cutoff),
+        cutoff_fn=          spk.nn.cutoff.CosineCutoff(cutoff=cutoff)
+    )
+    pred_U0 = spk.atomistic.Atomwise(n_in=n_atom_basis, output_key=QM9.U0)
+
+    nnpot = spk.model.NeuralNetworkPotential(
+        representation=painn,
+        input_modules=[pairwise_distance],
+        output_modules=[pred_U0],
+        postprocessors=[trn.CastTo64(), trn.AddOffsets(QM9.U0, add_mean=True, add_atomrefs=True)]
+    )
+
+    # Model Output
+    output_U0 = spk.task.ModelOutput(
+        name=QM9.U0,
+        loss_fn=torch.nn.MSELoss(),
+        loss_weight=1.,
+        metrics={
+            "MAE": torchmetrics.MeanAbsoluteError()
+        }
+    )
+
+    # Training Task
+    task = spk.task.AtomisticTask(
+        model=nnpot,
+        outputs=[output_U0],
+        optimizer_cls=torch.optim.AdamW,
+        optimizer_args={"lr": 1e-4}
+    )
+
+    # Training + Monitoring + Logging
+    logger = pl.loggers.TensorBoardLogger(save_dir=qm9atom)
+    callbacks = [
+        spk.train.ModelCheckpoint(
+            model_path=os.path.join(qm9atom, "best_inference_model"),
+            save_top_k=1,
+            monitor="val_loss"
+        )
+    ]
+
+    trainer = pl.Trainer(
+        callbacks=callbacks,
+        logger=logger,
+        default_root_dir=qm9atom,
+        max_epochs=3, # for testing, we restrict the number of epochs
+    )
+    trainer.fit(task, datamodule=qm9atom)
---
+++ run_experiment.py
@@ -0,0 +1,5 @@
+from experiments import qm9_atomwise
+
+if __name__ == '__main__':
+    
+    qm9_atomwise.run()
