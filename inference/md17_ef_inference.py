0d19cc473b172add90d0d137f2a77540ec0d92a5
Nico Kestel <=>
2023-11-24 18:17:49 +0100

    md17_ef inference fix

--- inference/md17_ef_inference.py
+++ inference/md17_ef_inference.py
@@ -1,6 +1,8 @@
 import os
+import copy
 
 import torch
+import torch.nn.functional as F
 from torch.utils.data import random_split
 
 import numpy as np
@@ -10,21 +12,30 @@ import schnetpack as spk
 import schnetpack.transform as trn
 
 from schnetpack.datasets.md17 import MD17
+# from schnetpack.data.loader import AtomsLoader
+from schnetpack.interfaces.ase_interface import SpkCalculator
+
+from schnetpack.units import convert_units
 
 from data_loader import load_data
 
 
 def run(path_to_model, path_to_data_dir, molecule='ethanol'):
     # set device
-    device = torch.device("cuda")
+    device = torch.device("cpu")
 
     # load model
     model_path = os.path.join(path_to_model)
     best_model = torch.load(model_path, map_location=device)
+    best_model.eval()
 
     # set up converter
-    converter = spk.interfaces.AtomsConverter(
-        neighbor_list=trn.ASENeighborList(cutoff=5.0), dtype=torch.float32, device=device
+    # converter = spk.interfaces.AtomsConverter(
+    #     neighbor_list=trn.ASENeighborList(cutoff=5.0), dtype=torch.float32, device=device
+    # )
+
+    calculator = spk.interfaces.SpkCalculator(
+        path_to_model, trn.ASENeighborList(cutoff=5.0), dtype=torch.float32, device=device
     )
 
     # load dataset
@@ -40,26 +51,36 @@ def run(path_to_model, path_to_data_dir, molecule='ethanol'):
                         n_val=50,  # 50
                         batch_size=10,
                         work_dir=path_to_data_dir)
+    
+    #print(dataset.test_idx)
 
     # create atoms object from dataset
-    splits = random_split(dataset.test_dataset, [1/3, 1/3, 1/3])
+    # splits = random_split(dataset.test_dataset, [1/3, 1/3, 1/3])
+    n_test = i = 10
+    # splits = dataset.test_dataset[:n_test//3], dataset.test_dataset[n_test//3 : 2*n_test//3], dataset.test_dataset[2*n_test//3:]
 
-    # compute MAE over 3 random splits
-    energy_avg_mae, forces_avg_mae = 0.0
-    for split in splits:
-        atoms = Atoms(
-            numbers=split[spk.properties.Z], positions=split[spk.properties.R]
-        )
+    # compute MAE
+    energy_avg_mae, forces_avg_mae = 0.0, 0.0
+    for batch in dataset.test_dataloader():
+        if i == 0:
+            break
 
-        # convert atoms to SchNetPack inputs and perform prediction
-        inputs = converter(atoms)
-        results = best_model(inputs)
+        print(batch['energy'])
+        results = best_model(copy.deepcopy(batch))
 
-        energy_avg_mae += torch.nn.L1Loss(results['energy'], inputs['energy'])
-        forces_avg_mae += torch.nn.L1Loss(results['forces'], inputs['forces'])
-    
-    energy_avg_mae /= len(splits)
-    forces_avg_mae /= len(splits)
+        # convert units
+        # forces stays same
+        results['energy'] *= convert_units("kcal/mol", "eV")
 
-    print('AVG (over 3 random splits) MAE on energy: {:.3f}'.format(energy_avg_mae))
-    print('AVG (over 3 random splits) MAE on forces: {:.3f}'.format(forces_avg_mae))
+        energy_avg_mae += F.l1_loss(results['energy'], batch['energy'])
+        forces_avg_mae += F.l1_loss(results['forces'], batch['forces'])
+        i -= 1
+
+
+    energy_avg_mae /= n_test
+    forces_avg_mae /= n_test
+
+    print('MAE on energy: {:.3f}'.format(
+        energy_avg_mae))
+    print('MAE on forces: {:.3f}'.format(
+        forces_avg_mae))
